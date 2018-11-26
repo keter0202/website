1e8bd6e9551e9b2088fbe515ab5a3d9aa939016b
keter <752761959@qq.com>
2018-11-27 00:05:34 +0800

    change

--- src/views/fan_ling/index.vue
+++ src/views/fan_ling/index.vue
@@ -2,27 +2,32 @@
   <div class="fan-ling" :style="{width: width, height: height}">
     <!-- 内容 -->
     <el-row v-if="view.viewBool">
-      <el-row class="top">
+      <el-row class="top" :gutter="20">
         <el-col :span="12">
-          <el-row>
-            <el-col :span="24">
-              <div class="logo">
-                <img src="../../assets/avatar_fl.jpg">
-              </div>
-              <p class="title">期待2022北京冬奥会</p>
-              <div class="all-button">
-                <k-button message="photos"></k-button>
-              </div>
-            </el-col>
-          </el-row>
+          <div class="logo">
+            <img src="../../assets/avatar_fl.jpg" @click="changeLogo" :class="{'active': changeLogoBool}">
+          </div>
         </el-col>
         <el-col :span="12">
-          <el-row v-if="isBirth">
-            生日快乐
-          </el-row>
-          <el-row v-else>
-            aaaaaa
-          </el-row>
+          <div v-if="isBirth" class="title">生日是谁，我不管它快不快乐，我只希望你快乐</div>
+          <div v-else class="title">today is not your birthday~</div>
+        </el-col>
+      </el-row>
+      <el-row class="middle">
+        <el-col :span="24">
+          <div>哈哈 还是祝你：</div>
+          <div class="text-in">越长越帅</div>
+          <div class="text-in">像个250</div>
+          <div class="text-in">多读书,多看报,少吃零食,多睡觉</div>
+          <div class="text-in">幸运常伴你左右</div>
+          <div class="text-in">是披荆斩棘的女英雄，也是被人疼爱的小朋友</div>
+          <div class="text-in">......</div>
+          <div>以上。</div>
+        </el-col>
+      </el-row>
+      <el-row class="middle">
+        <el-col :span="24">
+          <img src="../../assets/birth.png" alt="">
         </el-col>
       </el-row>
     </el-row>
@@ -58,7 +63,9 @@ export default {
       width: null, // 宽度
       height: null, // 高度
       timer: true, // 屏幕高宽控制
-      isBirth: false
+      isBirth: false,
+      arrList: [],
+      changeLogoBool: false
     }
   },
   created () {
@@ -76,8 +83,8 @@ export default {
     getWidthheight () {
       if (this.timer) {
         this.timer = false
-        let w = window.innerWidth
-        let h = window.innerHeight
+        let w = document.querySelector('body').clientWidth
+        let h = document.querySelector('body').clientHeight
         this.width = w + 'px'
         this.height = h + 'px'
         setTimeout(() => {
@@ -96,6 +103,9 @@ export default {
       } else {
         this.isBirth = false
       }
+    },
+    changeLogo () {
+      this.changeLogoBool = !this.changeLogoBool
     },
     // 检查密码
     checkPassword () {
@@ -126,8 +136,6 @@ export default {
       width: 80px;
       height: 80px;
       margin: 0 auto;
-      background-color: rgba(255, 255, 255, 0.3);
-      padding: 4px;
       border-radius: 50%;
       img{
         display: block;
@@ -136,23 +144,39 @@ export default {
         border-radius: 50%;
         transition: all .8s;
         cursor: pointer;
-        &:hover{
+        &.active{
           transform: rotateY(180deg);
         }
       }
     }
     .title{
-      margin: 0;
+      width: 100%;
       height: 80px;
-      line-height: 80px;
-      text-align: center;
+      // line-height: 80px;
       color: #fff;
     }
-    .all-button{
+  }
+  .middle{
+    width: 80%;
+    margin: 40px auto;
+    color: #fff;
+    .text-in{
+      text-indent: 2em;
+      line-height: 30px;
+    }
+    img{
+      display: block;
       width: 100px;
-      height: auto;
+      height: 100px;
       margin: 0 auto;
+      animation: birthMOve 3s;
+      animation-iteration-count: infinite;
     }
   }
+}
+@keyframes birthMOve {
+  0% {transform: translateY(0);}
+  50% {transform: translateY(20px);}
+  100% {transform: translateY(0);}
 }
 </style>
