04936ca27b61ab2e85af4a26755554b9256543ff
keter0202 <752761959@qq.com>
2019-01-08 18:06:03 +0800

    项目修改

---
+++ src/components/layout/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <vue-scroll :ops="ops">
+        <div class="logo">
+          <img src="@/assets/logo.png" width="63" height="63" alt="aitelife">
+        </div>
+        <Menu/>
+      </vue-scroll>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <el-row type="flex" justify="space-between" align="middle">
+          <el-col :span="12">
+            <Breadcrumb/>
+          </el-col>
+          <el-col :span="8" class="text-right">
+            <el-button @click="logout" type="text">退出</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <vue-scroll :ops="ops">
+        <el-main>
+          <router-view/>
+        </el-main>
+      </vue-scroll>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  import Menu from './Menu'
+  import Breadcrumb from './Breadcrumb'
+
+  export default {
+    name: 'Layout',
+    components: {
+      Menu,
+      Breadcrumb
+    },
+    data () {
+      return {
+        ops: {
+          scrollPanel: {
+            scrollingX: false
+          },
+          rail: {
+            vRail: {
+              width: '6px',
+              opacity: 1,
+              background: '#f2f2f2'
+            }
+          },
+          bar: {
+            vBar: {
+              keepShow: false,
+              background: '#c6c6c6'
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      // 登出css
+      logout () {
+        console.log('登出')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .el-aside {
+    height: 100%;
+    border-right: solid 1px #e6e9f0;
+    background-color: #f5f7fa;
+  }
+
+  .logo {
+    padding-top: 43px;
+    padding-bottom: 34px;
+    text-align: center;
+  }
+
+  .el-header {
+    border-bottom: 1px solid #e6e9f0;
+  }
+
+  .el-row {
+    height: 60px;
+  }
+
+  .user-center li a {
+    display: block;
+    text-align: center;
+    font-size: 16px;
+    line-height: 28px;
+    &:hover {
+      color: $blue;
+    }
+  }
+
+  /deep/ .vuescroll-content{
+    width: 100% !important;
+    height: 100% !important;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/home/index' // 首页
-import fanLing from '@/views/fan_ling/index' // fan ling
-import myself from '@/views/myself/index' // 我
+
+import layout from '@/components/layout/index'
+
+import home from '@/views/home/index' // 首页
 
 Vue.use(Router)
 
@@ -10,18 +11,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/fan-ling',
-      name: 'fanLing',
-      component: fanLing
-    },
-    {
-      path: '/liao-hang',
-      name: 'myself',
-      component: myself
+      name: 'layout',
+      component: layout,
+      redirect: 'home',
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: home
+        }
+      ]
     }
   ]
 })
